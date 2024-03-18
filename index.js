f80aa184c1da94dd861bef79670910d21c3aaa36
Pascal Marienfeld <pa.marienfeld@gmail.com>
2024-03-18 16:10:17 +0100

    reworked mongoose.connect to connect api with DB

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ app.use(cors({
     }
 }));
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const express = require('express'),
     bodyParser = require('body-parser'),
